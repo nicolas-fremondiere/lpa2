feb2fd97e92a90fe103d804a78153e8366931a8a
Nicolas Fremondiere <n.fremondiere@outlook.fr>
2020-03-11 14:38:45 +0100

    Issue 2
    API burgers

--- app/models/burger.rb
+++ app/models/burger.rb
@@ -1,2 +1,15 @@
 class Burger < ApplicationRecord
+    def getNurtiments
+        require 'openfoodfacts'
+        product = Openfoodfacts::Product.get(self.code, locale: 'fr')
+
+        # If no products then return nothing
+        if product == nil
+            product = [["Data missing"], [""]]
+          else
+            product = product.nutriments
+          end
+         
+          return product
+    end
 end
